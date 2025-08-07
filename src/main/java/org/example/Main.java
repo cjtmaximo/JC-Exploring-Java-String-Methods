a505a15ad0c0a9ec7290829242480c3d851f1d99
Calvin James Maximo <calvinjames.maximo@gcash.com>
2025-08-07 11:26:23 +0800

    feat: finish Exercise 4 - Changing Case

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,5 +16,12 @@ public class Main {
         String statement = "Java is powerful.";
         String part = statement.substring(5, 7);
         System.out.println("The extracted substring is: " + part); // "is"
+
+        // Exercise 4
+        String mixedCase = "This Is A Test";
+        String upper = mixedCase.toUpperCase();
+        String lower = mixedCase.toLowerCase();
+        System.out.println("Uppercase: " + upper);
+        System.out.println("Lowercase: " + lower);
     }
 }
